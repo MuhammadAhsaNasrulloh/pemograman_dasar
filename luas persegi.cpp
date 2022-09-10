ee36ce07182c960afa3d89a01ca47be34efa6868
Muhammad Ahsan Nasrulloh <106051351+MuhammadAhsaNasrulloh@users.noreply.github.com>
2022-09-10 20:28:50 +0700

    Add files via upload

---
+++ luas persegi.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int sisi;
+	int luaspersegi;
+ 	sisi = 5;
+ 	cout << "sisi = " <<sisi <<endl;
+ 	luaspersegi = sisi * sisi;
+ 	cout <<"luas-persegi = " << luaspersegi << endl;
+}
